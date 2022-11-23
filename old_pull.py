86e070ef3533d4a07f75bb58ea9502389ba0063a
Fenwayb <fenwayb@gmail.com>
2022-11-22 23:10:58 -0500

    testing file path issues

--- old_pull.py
+++ old_pull.py
@@ -8,11 +8,12 @@ import sqlite3
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from splinter import Browser
-from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
+# from splinter import Browser
+# from bs4 import BeautifulSoup as soup
+# from webdriver_manager.chrome import ChromeDriverManager
 
-
+# home = "Boston Bruins"
+# away = "New Jersey Devils"
 
 def pull_all(home, away):
     # home = home
@@ -23,15 +24,15 @@ def pull_all(home, away):
     # cur.execute("CREATE TABLE home(pppctg, pkpctg, shots, shotsallowed, faceofpctg, shootingpctg, savepctg)")
     # cur.execute("CREATE TABLE away(pppctg, pkpctg, shots, shotsallowed, faceofpctg, shootingpctg, savepctg)")
     # cur.execute("CREATE TABLE results(results)")
-    X = pd.read_csv('C:/Users/fenwa/Desktop/Git/HockeyTradeProject/Resources/X.csv', index_col=1)
-    team_info_df = pd.read_csv('C:/Users/fenwa/Desktop/Git/HockeyTradeProject/Resources/team_info.csv', index_col=1)
+    X = pd.read_csv('Resources/X.csv', index_col=1)
+    team_info_df = pd.read_csv('resources/team_info.csv', index_col=1)
     team_info_df['teamName'] = team_info_df['shortName'] + " " + team_info_df['teamName']
     url = "https://statsapi.web.nhl.com/api/v1/teams"
     #scraping our own website for ids - is this the right way to do it?
     # idurl = "http://127.0.0.1:5000/"
     # executable_path = {'executable_path': ChromeDriverManager().install()}
     # browser = Browser('chrome', **executable_path, headless=True)
-    def pull_home(home):
+    def pull_home():
         # home = home
         home_id = team_info_df[team_info_df['teamName'] == f'{home}']
         home_name = home_id['teamName'].values[0]
@@ -58,7 +59,8 @@ def pull_all(home, away):
         # con.commit()
         return home_df, home_name
     home_df, home_name = pull_home(home)
-    def pull_away(away):
+
+    def pull_away():
         # away = away
         away_id = team_info_df[team_info_df['teamName'] == f"{away}"]
         away_name = away_id['teamName'].values[0]
@@ -89,7 +91,7 @@ def pull_all(home, away):
     compiled_stats_df = pd.concat([home_df,away_df],axis=1)
     scaler = MinMaxScaler().fit(X)
     compiled_scaled_df = pd.DataFrame(scaler.transform(compiled_stats_df),columns = compiled_stats_df.columns)
-    with open('C:/Users/fenwa/Desktop/Git/HockeyTradeProject/Resources/HockeyMLmodel.pkl', 'rb') as f:
+    with open('Resources/HockeyMLmodel.pkl', 'rb') as f:
         clf2 = pickle.load(f)
         results = clf2.predict(compiled_scaled_df[0:1])
         if results[0] == "0":
@@ -102,5 +104,3 @@ def pull_all(home, away):
     return home_df, away_df, results
 
 home_df, away_df, results = pull_all("Boston Bruins", "New Jersey Devils")
-print(home_df, away_df, results)
-
