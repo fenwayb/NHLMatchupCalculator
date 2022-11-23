1cb66ecdef408e926d3110e29d76a1629ac3621d
Josh Stowe <Josh.Stowe@patersoncooke.com>
2022-11-22 22:28:23 -0700

    group code commit

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, redirect, request
 from pull import pull_all
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = "template")
 
 @app.route("/")
 def index():
    return render_template("index.html")
 
-app.route("/pull", method=["GET", "POST"])
+@app.route("/pull", methods=["GET", "POST"])
 def pull():
-   if request.method == 'GET':
+   if request.method == 'POST':
          home = id.home
          away = id.away
    home_df, away_df, results = pull_all(home, away)
@@ -20,7 +20,8 @@ def pull():
    #away_df to a table under away box
    #result to where ever we want it
    
-   return redirect('/', code=302)
-
+   # return redirect('/', code=302)
+   
+   return render_template('index.html',results = results)
 if __name__ == "__main__":
     app.run()
---
+++ static/js/getid.js
@@ -0,0 +1,4 @@
+function id (){
+    let home = document.getElementById("Home").value()
+    let away = document.getElementById("Away").value()
+}
